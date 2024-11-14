a85161466258790fa91daa1bb3314b57b362fb6f
naufalmiftakhul <miftakhulnaufal08.@gmail.com>
2024-11-14 20:21:32 +0700

    Add files via upload

---
+++ nopalindi.py
@@ -0,0 +1,14 @@
+phi = 3.14
+
+def luas_lingkaran():
+  jari = float(input("Masaukkan jari-jari lingkaran: "))
+  return phi * jari ** 2
+
+def luas_persegi():
+  sisi = float(input("Masukkan sisi persegi: "))
+  return sisi ** 2
+
+def luas_segitiga():
+  alas = float(input("Masukkan alas segitiga: "))
+  tinggi = float(input("Masukkan tinggi segitiga: "))
+  return alas * tinggi
